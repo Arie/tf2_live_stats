fd1d59a3555fb825a381a04457e94deff3a4b4e5
Arie <git@ariekanarie.nl>
2014-12-14 22:34:52 +0100

    add way more weapons

--- app/decorators/tf2_line_parser/events/kill_decorator.rb
+++ app/decorators/tf2_line_parser/events/kill_decorator.rb
@@ -13,9 +13,28 @@ class TF2LineParser::Events::KillDecorator < TF2LineParser::PvpEventDecorator
   end
 
   def interesting_weapon?
-     [ "blutsauger", "proto_syringe", "ubersaw", "crusaders_crossbow",
-       "sniperrifle", "awper_hand", "machina", "classic", "hitman_heatmaker",
-       "knife", "black_rose"].include?(weapon)
+    [
+    #Scout
+    "bat", "bat_fish", "bat_wood", "atomizer", "ball", "boston_basher", "cleaver", "guilotine", "holy_mackerel", "sandman", "scout_sword", "warfan",
+    #Soldier
+    "unique_pickaxe_escape", "shovel", "disciplinary_action", "mantreads", "market_gardener", "taunt_soldier",
+    #Pyro
+    "deflect_rocket", "deflect_arrow", "deflect_promode", "fireaxe", "powerjack", "taunt_pyro",
+    #Demoman
+    "ullapool_caber_explosion", "ullapool_caber", "bottle", "club",
+    #Heavy
+    "steel_fists", "fists", "taunt_heavy",
+    #Engineer
+    "mechanical_arm", "southern_hospitality", "taunt_engineer", "wrench", "wrench_jag",
+    #Medic
+     "blutsauger", "proto_syringe", "ubersaw", "bonesaw", "crusaders_crossbow", "syringegun_medic", "taunt_medic",
+    #Sniper
+    "sniperrifle", "awper_hand", "machina", "classic", "hitman_heatmaker", "bazaar_bargain", "bushwacka", "sniperrifle_classic", "sniperrifle_decap", "taunt_sniper",
+    #Spy
+    "knife", "black_rose", "spy_cicle", "voodoo_pin",
+    #Multi
+    "freedom_staff", "fryingpan", "paintrain",
+    ].include?(weapon)
   end
 
 end

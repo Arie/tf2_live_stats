7d232cb85b179664714146797b2c6bc3cfdd7398
Arie <github@ariekanarie.nl>
2023-12-25 22:47:41 +0100

    Update rcon_command_decorator.rb

--- app/decorators/tf2_line_parser/events/rcon_command_decorator.rb
+++ app/decorators/tf2_line_parser/events/rcon_command_decorator.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
-class TF2LineParser::Events::RconCommandDecorator < TF2LineParser::UnknownDecorator
+class TF2LineParser::RconCommandDecorator < TF2LineParser::EventDecorator
+
+  def message_text
+    icon('icon-comment')
+  end
+
+  def icon_text
+    content_tag(:em, message)
+  end
 end
